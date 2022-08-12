440a5a7747898383c0b4ce7afe14252bbc34fbdf
Yurun <admin@yurunsoft.com>
2022-08-12 20:07:19 +0800

    test

--- src/Components/swoole/src/Server/Util/LocalServerUtil.php
+++ src/Components/swoole/src/Server/Util/LocalServerUtil.php
@@ -643,45 +643,45 @@ class LocalServerUtil implements ISwooleServerUtil
      */
     public function getConnectionCount(?string $serverName = null): int
     {
-        $server = $this->getServer($serverName);
-        $swooleServer = $server->getSwooleServer();
-        if (\SWOOLE_BASE === $swooleServer->mode)
-        {
-            $result = 0;
-            $id = uniqid('', true);
-            try
-            {
-                $channel = ChannelContainer::getChannel($id);
-                $count = $this->sendMessage('getConnectionCountRequest', [
-                    'messageId'    => $id,
-                    'serverName'   => $server->getName(),
-                    'needResponse' => true,
-                ]);
-                if (ProcessType::PROCESS !== App::get(ProcessAppContexts::PROCESS_TYPE))
-                {
-                    for ($i = $count; $i > 0; --$i)
-                    {
-                        $popResult = $channel->pop($this->waitResponseTimeout);
-                        var_dump(__METHOD__, $popResult);
-                        if (false === $popResult)
-                        {
-                            break;
-                        }
-                        $result += ($popResult['result'] ?? 0);
-                    }
-                }
+        // $server = $this->getServer($serverName);
+        // $swooleServer = $server->getSwooleServer();
+        // if (\SWOOLE_BASE === $swooleServer->mode)
+        // {
+        //     $result = 0;
+        //     $id = uniqid('', true);
+        //     try
+        //     {
+        //         $channel = ChannelContainer::getChannel($id);
+        //         $count = $this->sendMessage('getConnectionCountRequest', [
+        //             'messageId'    => $id,
+        //             'serverName'   => $server->getName(),
+        //             'needResponse' => true,
+        //         ]);
+        //         if (ProcessType::PROCESS !== App::get(ProcessAppContexts::PROCESS_TYPE))
+        //         {
+        //             for ($i = $count; $i > 0; --$i)
+        //             {
+        //                 $popResult = $channel->pop($this->waitResponseTimeout);
+        //                 var_dump(__METHOD__, $popResult);
+        //                 if (false === $popResult)
+        //                 {
+        //                     break;
+        //                 }
+        //                 $result += ($popResult['result'] ?? 0);
+        //             }
+        //         }
 
-                return $result;
-            }
-            finally
-            {
-                ChannelContainer::removeChannel($id);
-            }
-        }
-        else
-        {
-            return $server->getSwoolePort()->connections->count();
-        }
+        //         return $result;
+        //     }
+        //     finally
+        //     {
+        //         ChannelContainer::removeChannel($id);
+        //     }
+        // }
+        // else
+        // {
+        return $server->getSwoolePort()->connections->count();
+        // }
     }
 
     /**
